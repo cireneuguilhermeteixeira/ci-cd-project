85bc7ab10fbdfeb90a57a4585cc107699fca2dec
Cireneu Guilherme <cdearaujo@lendingclub.com>
2023-03-30 08:18:20 -0300

    Adjusting format to break test

--- _tests_/unit_test/test-pages.spec.js
+++ _tests_/unit_test/test-pages.spec.js
@@ -5,7 +5,7 @@ const srvc = require('../../app/service');
 describe ('Main test', function() {
     it('Should test', function(done){
         const formatedDate = srvc.getCurrentDateFormated(796089600000);
-        expect(formatedDate).to.equal("March 24, 1995 - 09:00:00");
+        expect(formatedDate).to.equal("March 24, 1995 - 09:10:00");
         done();
     });        
 });
